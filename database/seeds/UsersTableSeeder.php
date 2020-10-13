5212b804a0f78fa701e7a5b627e988fc1194c0d0
Ary arfani <aryarfani@gmail.com>
2020-10-13 12:44:49 +0700

    delete api_token column

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,24 +12,21 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('users')->delete();
-        
-        \DB::table('users')->insert(array (
-            0 => 
-            array (
+
+        \DB::table('users')->insert(array(
+            0 =>
+            array(
                 'id' => 1,
                 'nik' => '123235243',
                 'username' => 'bambang',
                 'image' => NULL,
                 'password' => '$2y$10$Qmcv27JWOWH7ds9.v7vUhOuPUbzi7qnlmzksD4AhF6l/H1u8cADta',
-                'api_token' => NULL,
                 'created_at' => '2020-10-06 16:34:56',
                 'updated_at' => '2020-10-06 16:34:56',
             ),
         ));
-        
-        
     }
 }
